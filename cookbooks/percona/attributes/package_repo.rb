8747c5a78f8607fdef26103c270c4c811520a6dd
Klemen Novak <klemen@kinkcreative.com>
2014-06-01 23:25:34 -0700

    Added cookbooks

---
+++ cookbooks/percona/attributes/package_repo.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: percona
+# Attributes:: package_repo
+#
+
+default["percona"]["use_percona_repos"] = true
+default["percona"]["apt_uri"] = "http://repo.percona.com/apt"
+default["percona"]["apt_keyserver"] = "keys.gnupg.net"
+default["percona"]["apt_key"] = "CD2EFD2A"
---
+++ cookbooks/percona/recipes/server.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: percona
+# Recipe:: server
+#
+
+include_recipe "percona::package_repo"
+
+# install packages
+case node["platform_family"]
+when "debian"
+  package node["percona"]["server"]["package"] do
+    action :install
+    options "--force-yes"
+  end
+when "rhel"
+  # Need to remove this to avoid conflicts
+  package "mysql-libs" do
+    action :remove
+    not_if "rpm -qa | grep #{node["percona"]["server"]["shared_pkg"]}"
+  end
+
+  # we need mysqladmin
+  include_recipe "percona::client"
+
+  package node["percona"]["server"]["package"] do
+    action :install
+  end
+end
+
+unless node["percona"]["skip_configure"]
+  include_recipe "percona::configure_server"
+end
+
+# access grants
+unless node["percona"]["skip_passwords"]
+  include_recipe "percona::access_grants"
+  include_recipe "percona::replication"
+end
